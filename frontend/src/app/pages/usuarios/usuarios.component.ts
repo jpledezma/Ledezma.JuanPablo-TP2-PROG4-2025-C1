a8c33f453b2f81ee4bdba7deac2f8e6d2a6ab751
jpledezma <jpledezma01@outlook.com>
2025-06-26 13:07:51 -0300

    feat(front): se agrega una tabla de control de usuarios

--- frontend/src/app/pages/usuarios/usuarios.component.ts
+++ frontend/src/app/pages/usuarios/usuarios.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import {
   FormControl,
@@ -8,19 +8,35 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { UsuariosService } from '../../services/usuarios.service';
 import Swal from 'sweetalert2';
+import { DatePipe } from '@angular/common';
+
+interface Usuario {
+  _id: string;
+  nombre: string;
+  apellido: string;
+  email: string;
+  username: string;
+  urlFotoPerfil: string;
+  urlFotoThumbnail: string;
+  createdAt: number;
+  eliminado: boolean;
+  descripcion?: string;
+  acceso?: string;
+}
 
 @Component({
   selector: 'app-usuarios',
-  imports: [HeaderComponent, FormsModule, ReactiveFormsModule],
+  imports: [HeaderComponent, FormsModule, ReactiveFormsModule, DatePipe],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css',
 })
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit {
   formulario: FormGroup;
   router = inject(Router);
-  authService = inject(AuthService);
+  usuariosService = inject(UsuariosService);
+  usuarios?: Usuario[] | undefined;
 
   constructor() {
     this.formulario = new FormGroup({
@@ -34,6 +50,65 @@ export class UsuariosComponent {
         Validators.pattern(/^[\S]{4,}$/),
       ]),
     });
+  }
+
+  async ngOnInit() {
+    const usuarios = await this.usuariosService.obtenerUsuarios();
+    if (usuarios) {
+      this.usuarios = usuarios;
+    } else {
+      this.usuarios = [];
+    }
+  }
+
+  ordenarTabla(atributo: keyof Usuario, invertido: boolean = false) {
+    if (invertido) {
+      this.usuarios?.sort((a: Usuario, b: Usuario) =>
+        a[atributo]! < b[atributo]! ? 1 : -1,
+      );
+    } else {
+      this.usuarios?.sort((a: Usuario, b: Usuario) =>
+        a[atributo]! > b[atributo]! ? 1 : -1,
+      );
+    }
+  }
+
+  async eliminarUsuario(id: string) {
+    const respuesta = await this.mostrarMensajePregunta('eliminar');
+    if (!respuesta) {
+      return;
+    }
+
+    const exito = await this.usuariosService.eliminarUsuario(id);
+
+    if (exito) {
+      this.mostrarMensajeExito('elimino');
+      const usuarioEliminado = this.usuarios?.find(
+        (usuario) => usuario._id === id,
+      );
+      usuarioEliminado!.eliminado = true;
+    } else {
+      this.mostrarMensajeError('eliminar');
+    }
+  }
+
+  async restaurarUsuario(id: string) {
+    const respuesta = await this.mostrarMensajePregunta('restaurar');
+    if (!respuesta) {
+      return;
+    }
+
+    const exito = await this.usuariosService.restaurarUsuario(id);
+
+    if (exito) {
+      this.mostrarMensajeExito('restauro');
+      const usuarioRestaurado = this.usuarios?.find(
+        (usuario) => usuario._id === id,
+      );
+      usuarioRestaurado!.eliminado = false;
+    } else {
+      this.mostrarMensajeError('restaurar');
+    }
   }
 
   async guardarUsuario() {
@@ -50,7 +125,6 @@ export class UsuariosComponent {
       return;
     }
 
-    // crear usuario
     const formData = new FormData();
     formData.append('nombre', this.formulario.value.nombre.trim());
     formData.append('apellido', this.formulario.value.apellido.trim());
@@ -62,31 +136,59 @@ export class UsuariosComponent {
       new Date(this.formulario.value.fechaNacimiento).getTime().toString(),
     );
 
-    const exito: any = await this.authService.registrar(formData);
+    const exito = await this.usuariosService.crearUsuario(formData);
 
     if (exito) {
-      Swal.fire({
-        icon: 'success',
-        text: 'Se creó el usuario',
-        theme: 'dark',
-        width: '50rem',
-        customClass: {
-          htmlContainer: 'modal-texto',
-          confirmButton: 'modal-boton',
-        },
-      });
+      this.mostrarMensajeExito('creo');
     } else {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'No se pudo crear el usuario',
-        theme: 'dark',
-        width: '50rem',
-        customClass: {
-          htmlContainer: 'modal-texto',
-          confirmButton: 'modal-boton',
-        },
-      });
+      this.mostrarMensajeError('crear');
     }
+  }
+
+  async mostrarMensajePregunta(accion: 'eliminar' | 'restaurar') {
+    const resultado = await Swal.fire({
+      icon: 'question',
+      text: `¿Seguro que quieres ${accion} al usuario?`,
+      theme: 'dark',
+      width: '50rem',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: accion,
+      customClass: {
+        title: 'modal-titulo',
+        htmlContainer: 'modal-texto',
+        icon: 'modal-icono',
+        confirmButton: 'modal-boton',
+        cancelButton: 'modal-boton-cancelar',
+      },
+    });
+
+    return resultado.isConfirmed;
+  }
+
+  async mostrarMensajeExito(accion: 'elimino' | 'creo' | 'restauro') {
+    Swal.fire({
+      icon: 'success',
+      text: `Se ${accion} el usuario`,
+      theme: 'dark',
+      width: '50rem',
+      customClass: {
+        htmlContainer: 'modal-texto',
+        confirmButton: 'modal-boton',
+      },
+    });
+  }
+
+  async mostrarMensajeError(accion: 'eliminar' | 'crear' | 'restaurar') {
+    Swal.fire({
+      icon: 'success',
+      text: `No se pudo ${accion} el usuario`,
+      theme: 'dark',
+      width: '50rem',
+      customClass: {
+        htmlContainer: 'modal-texto',
+        confirmButton: 'modal-boton',
+      },
+    });
   }
 }
--- frontend/src/app/services/usuarios.service.ts
+++ frontend/src/app/services/usuarios.service.ts
@@ -15,6 +15,17 @@ export class UsuariosService {
 
   async crearUsuario(usuario: any) {
     const url = environment.url + 'auth/registro';
+    let exito;
+    const peticion = this.httpClient.post(url, usuario);
+    try {
+      await firstValueFrom(peticion);
+      exito = true;
+    } catch (error) {
+      exito = false;
+      console.log(error);
+    }
+
+    return exito;
   }
 
   async modificarUsuario(modificaciones: any, id: string) {
@@ -26,6 +37,43 @@ export class UsuariosService {
     } catch (error) {
       console.log(error);
       return false;
+    }
+  }
+
+  async eliminarUsuario(id: string) {
+    const url = this.url + id;
+    const peticion = this.httpClient.delete(url);
+    try {
+      await firstValueFrom(peticion);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async restaurarUsuario(id: string) {
+    const url = this.url + 'restore';
+    const peticion = this.httpClient.post(url, { id });
+    try {
+      await firstValueFrom(peticion);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async obtenerUsuarios() {
+    const url = this.url;
+    const peticion = this.httpClient.get(url);
+    try {
+      const respuesta: any = await firstValueFrom(peticion);
+
+      return respuesta.payload;
+    } catch (error) {
+      console.log(error);
+      return null;
     }
   }
 }
